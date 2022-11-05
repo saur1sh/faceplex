c88e160b7031218e608971ee9923e898a28a2202
Saurish Sharma <isaurishsharma@gmail.com>
2022-11-05 11:41:04 +0530

    commit 2

---
+++ faceplex/front/src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Tilt from 'react-tilt'; 
+import Tilt from 'react-parallax-tilt';
+import face from './face.png';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br20 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={face}/>
+        </div>
+      </Tilt>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ faceplex/front/src/components/Navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if (isSignedIn) {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signout')} className='f4 link dim black pa3 pointer ba b--black bw2 mr2' style={{backgroundColor: "#C1E3ED", borderWidth: "3px",borderColor: "black", borderRadius: "30px"}}>Sign Out</p>
+        </nav>
+      );
+    } else {
+      return (
+        <div>          
+          <nav style={{display: 'flex'}}>
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+              <p onClick={() => onRouteChange('about')} className='f4 link dim black pa3 pointer ba b--black bw2 mr3 ml3 .bg-white' style={{backgroundColor: "#C1E3ED", borderWidth: "3px",borderColor: "black", borderRadius: "30px"}}>About</p>
+              <p onClick={() => onRouteChange('deveopers')} className='f4 link dim black pa3 pointer ba b--black mr2 ' style={{backgroundColor: "#C1E3ED", borderWidth: "3px",borderColor: "black", borderRadius: "30px"}}>Developers</p>
+            </div>
+            <div style={{marginLeft: 'auto' }}>
+              <p onClick={() => onRouteChange('signin')} className='f4 link dim black pa3 pointer ba b--black bw2 mr3' style={{backgroundColor: "#C1E3ED", borderWidth: "3px",borderColor: "black", borderRadius: "30px"}}>Sign In</p>
+            </div>
+              <p onClick={() => onRouteChange('register')} className='f4 link dim black pa3 pointer ba b--black mr3' style={{backgroundColor: "#C1E3ED", borderWidth: "3px",borderColor: "black", borderRadius: "30px"}}>Register</p>
+          </nav>
+        </div>
+      );
+    }
+}
+
+export default Navigation;
