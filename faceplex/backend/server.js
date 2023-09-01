a85c3cf5d1f103004e384c22cb8ba9c17c460a85
Saurish <isaurishsharma@gmail.com>
2023-09-01 19:09:12 +0530

    Update server.js

--- faceplex/backend/server.js
+++ faceplex/backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
